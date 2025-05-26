e8bf373f4014c35c702406afa2dae4732c57cb33
Daniel Samson <12231216+daniel-samson@users.noreply.github.com>
2025-05-26 09:50:06 +0100

    adding icons to menus

--- src/main/java/media/samson/jamplate/HelloController.java
+++ src/main/java/media/samson/jamplate/HelloController.java
@@ -2,7 +2,9 @@ package media.samson.jamplate;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.Tooltip;
+import javafx.scene.paint.Color;
 import org.controlsfx.control.action.Action;
 import org.controlsfx.glyphfont.FontAwesome;
 import org.controlsfx.glyphfont.Glyph;
@@ -16,17 +18,49 @@ public class HelloController {
     @FXML private Button btnCut;
     @FXML private Button btnCopy;
     @FXML private Button btnPaste;
+    
+    @FXML private MenuItem menuNew;
+    @FXML private MenuItem menuOpen;
+    @FXML private MenuItem menuSave;
+    @FXML private MenuItem menuCut;
+    @FXML private MenuItem menuCopy;
+    @FXML private MenuItem menuPaste;
 
     @FXML
     public void initialize() {
         GlyphFont fontAwesome = GlyphFontRegistry.font("FontAwesome");
         
-        btnNew.setGraphic(fontAwesome.create(FontAwesome.Glyph.FILE).size(16));
-        btnOpen.setGraphic(fontAwesome.create(FontAwesome.Glyph.FOLDER_OPEN).size(16));
-        btnSave.setGraphic(fontAwesome.create(FontAwesome.Glyph.SAVE).size(16));
-        btnCut.setGraphic(fontAwesome.create(FontAwesome.Glyph.CUT).size(16));
-        btnCopy.setGraphic(fontAwesome.create(FontAwesome.Glyph.COPY).size(16));
-        btnPaste.setGraphic(fontAwesome.create(FontAwesome.Glyph.PASTE).size(16));
+        // Create glyphs for toolbar buttons (using default color)
+        Glyph fileGlyph = fontAwesome.create(FontAwesome.Glyph.FILE).size(16);
+        Glyph folderOpenGlyph = fontAwesome.create(FontAwesome.Glyph.FOLDER_OPEN).size(16);
+        Glyph saveGlyph = fontAwesome.create(FontAwesome.Glyph.SAVE).size(16);
+        Glyph cutGlyph = fontAwesome.create(FontAwesome.Glyph.CUT).size(16);
+        Glyph copyGlyph = fontAwesome.create(FontAwesome.Glyph.COPY).size(16);
+        Glyph pasteGlyph = fontAwesome.create(FontAwesome.Glyph.PASTE).size(16);
+        
+        // Create separate glyphs for menu items with darker color
+        Glyph menuFileGlyph = fontAwesome.create(FontAwesome.Glyph.FILE).size(16).color(Color.GRAY);
+        Glyph menuFolderOpenGlyph = fontAwesome.create(FontAwesome.Glyph.FOLDER_OPEN).size(16).color(Color.GRAY);
+        Glyph menuSaveGlyph = fontAwesome.create(FontAwesome.Glyph.SAVE).size(16).color(Color.GRAY);
+        Glyph menuCutGlyph = fontAwesome.create(FontAwesome.Glyph.CUT).size(16).color(Color.GRAY);
+        Glyph menuCopyGlyph = fontAwesome.create(FontAwesome.Glyph.COPY).size(16).color(Color.GRAY);
+        Glyph menuPasteGlyph = fontAwesome.create(FontAwesome.Glyph.PASTE).size(16).color(Color.GRAY);
+        
+        // Set icons on toolbar buttons
+        btnNew.setGraphic(fileGlyph);
+        btnOpen.setGraphic(folderOpenGlyph);
+        btnSave.setGraphic(saveGlyph);
+        btnCut.setGraphic(cutGlyph);
+        btnCopy.setGraphic(copyGlyph);
+        btnPaste.setGraphic(pasteGlyph);
+        
+        // Set menu item icons with light-colored glyphs for better visibility in dark themes
+        menuNew.setGraphic(menuFileGlyph);
+        menuOpen.setGraphic(menuFolderOpenGlyph);
+        menuSave.setGraphic(menuSaveGlyph);
+        menuCut.setGraphic(menuCutGlyph);
+        menuCopy.setGraphic(menuCopyGlyph);
+        menuPaste.setGraphic(menuPasteGlyph);
 
         // Optional: Set tooltips
         btnNew.setTooltip(new Tooltip("New File"));
